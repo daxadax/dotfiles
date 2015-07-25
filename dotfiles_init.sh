7be139ef125c9fe0e21f2315a6e4e80f7ced0ef7
dax <d.dax@email.com>
2015-07-25 20:31:11 +0200

    Add gemrc to dotfiles

--- dotfiles_init.sh
+++ dotfiles_init.sh
@@ -7,7 +7,7 @@ dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 
 files="bash_profile gitconfig i3status.conf screenrc bashrc i3config profile
-xinitrc vimrc default_packages"   
+xinitrc vimrc default_packages gemrc"   
 
 folders="vim aliases"
 
