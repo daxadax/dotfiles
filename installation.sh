6f742165734f21e606f8fcf30220f4358ba52652
dax <d.dax@email.com>
2015-08-02 10:11:00 +0000

    Remove udiskie permissions from install script

--- installation.sh
+++ installation.sh
@@ -16,15 +16,6 @@ pkgfile --update
 rmmod pcspkr
 echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
 
-# add udiskie permissions for 'wheel' members
-mkdir - p /etc/polkit-1/localauthority/50-local.d/
-local udiskie_file='/etc/polkit-1/localauthority/50-local.d/10-udiskie.pkla'
-echo '[udiskie]' >> udiskie_file
-echo 'Identity=unix-group:wheel' >> udiskie_file
-echo
-'Action=org.freedesktop.udisks.filesystem-mount;org.freedesktop.udisks.drive-eject;org.freedesktop.udisks.drive-detach' >> udiskie_file
-echo 'ResultAny=yes' >> udiskie_file
-
 # create vim directories
 mkdir $HOME/.vim/backup
 mkdir $HOME/.vim/undo
