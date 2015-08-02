c076e07c639b259da5e6a171efbe460a17ce8092
dax <d.dax@email.com>
2015-08-02 09:37:45 +0000

    Add udiskie and dunst

--- installation.sh
+++ installation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # clone necessary files
-#git clone https://github.com/daxadax/dotfiles.git
-#cd dotfiles
+git clone https://github.com/daxadax/dotfiles.git
+cd dotfiles
 ./dotfiles_init.sh
 
 
@@ -15,6 +15,15 @@ pkgfile --update
 # turn off pcspeaker beep
 rmmod pcspkr
 echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
+
+# add udiskie permissions for 'wheel' members
+mkdir - p /etc/polkit-1/localauthority/50-local.d/
+local udiskie_file='/etc/polkit-1/localauthority/50-local.d/10-udiskie.pkla'
+echo '[udiskie]' >> udiskie_file
+echo 'Identity=unix-group:wheel' >> udiskie_file
+echo
+'Action=org.freedesktop.udisks.filesystem-mount;org.freedesktop.udisks.drive-eject;org.freedesktop.udisks.drive-detach' >> udiskie_file
+echo 'ResultAny=yes' >> udiskie_file
 
 # create vim directories
 mkdir $HOME/.vim/backup
